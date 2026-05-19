f905bdd35c44c1ca469792c8c86c338c10e28df1
Tuan Ngoc Chu <tchu0046@student.monash.edu>
2026-05-20 00:20:33 +1000

    first 5 inital chart

---
+++ vega_lite_vis.js
@@ -0,0 +1,5 @@
+vegaEmbed("#map-venues", "vega/map_venues.json").catch(console.error);
+vegaEmbed("#map-choropleth", "vega/map_choropleth.json").catch(console.error);
+vegaEmbed("#chart-line", "vega/line_aussie_share.json").catch(console.error);
+vegaEmbed("#chart-lollipop", "vega/lollipop_number1.json").catch(console.error);
+vegaEmbed("#chart-bump", "vega/bump_rankings.json").catch(console.error);
