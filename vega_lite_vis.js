e5930083db34ecd69ed5a342da64c3a771f8549c
Tuan Ngoc Chu <tchu0046@student.monash.edu>
2026-05-20 11:31:10 +1000

    update

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -3,3 +3,4 @@ vegaEmbed("#map-choropleth", "vega/map_choropleth.json").catch(console.error);
 vegaEmbed("#chart-line", "vega/line_aussie_share.json").catch(console.error);
 vegaEmbed("#chart-lollipop", "vega/lollipop_number1.json").catch(console.error);
 vegaEmbed("#chart-bump", "vega/bump_rankings.json").catch(console.error);
+vegaEmbed("#chart-pie", "vega/bar_singles_albums.json").catch(console.error);
